2fc3bc171ddd3e15b80f66c5173eb35ec43a4b4c
Mats Rietdijk <matsrietdijk@gmail.com>
2015-01-08 12:34:04 +0100

    Adds more default files

---
+++ PACKAGE/MODEL/Message.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module %PACKAGE%.%MODEL%.Message
+       ( %MODEL%Message (..)
+       , defaultMessage
+         -- * All languages
+       , englishMessage
+       ) where
+
+import           Data.Monoid (mappend)
+import           Data.Text   (Text)
+
+data %MODEL%Message =
+    Menu%MODEL%
+  | %MODEL%Index
+  | New%MODEL%
+  | Edit%MODEL%
+  | SaveSuccess
+  | UpdateSuccess
+  | DeleteSuccess
+  | Title
+  | Save
+
+defaultMessage :: %MODEL%Message -> Text
+defaultMessage = englishMessage
+
+englishMessage :: %MODEL%Message -> Text
+englishMessage Menu%MODEL%           = "%MODEL%s"
+englishMessage New%MODEL%            = "New %LC_MODEL%"
+englishMessage Edit%MODEL%           = "Edit %LC_MODEL%"
+englishMessage SaveSuccess           = "Successfully saved"
+englishMessage UpdateSuccess         = "Successfully updated"
+englishMessage DeleteSuccess         = "Successfully deleted"
+englishMessage Title                 = "Title"
+englishMessage Save                  = "Save"
