2eccaed209b45fbb97fbae4eb544939724217f5e
Cies Breijs <cies AT kde DOT nl>
2015-07-18 00:23:11 +0200

    Fix lambdacms-core version in stack.yaml

--- create_patches.sh
+++ create_patches.sh
@@ -1,32 +1,30 @@
 #!/bin/sh
 
-# ./create_patches.sh <unpatched app root> <patched app root> <patch files go here>
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <unpatched app root> <patched app root> <patch files go here>"
+  exit 1
+fi
 
 UNPATCHED=$1
 PATCHED=$2
 KEEP=$3
 
-REGULAR_FILES="Foundation.hs Application.hs Model.hs Roles.hs Settings.hs config/routes config/models config/settings.yml"
-CABAL_FILE=`basename $1/*.cabal`
+REGULAR_FILES="Foundation.hs Application.hs Model.hs Roles.hs Settings.hs config/routes config/models config/settings.yml stack.yaml"
+CABAL_FILE=`basename $UNPATCHED/*.cabal`
 
-
-combined_file=$KEEP/all_patches_combined.patch
-echo "" > $combined_file
-
-for f in $REGULAR_FILES
-  do target_file=$KEEP/`echo $f | sed "y/\//_/"`.patch
-     diff -Nur $UNPATCHED/$f $PATCHED/$f > $target_file
-     sed -i "s|$UNPATCHED|a|" $target_file
-     sed -i "s|$PATCHED|b|" $target_file
-     echo "diff -urN a/$f b/$f" >> $combined_file
-     cat $target_file >> $combined_file
+# regular files
+for f in $REGULAR_FILES; do
+  target_file=$KEEP/`echo $f | sed "y/\//_/"`.patch
+  diff -Nur $UNPATCHED/$f $PATCHED/$f > $target_file
+  sed -i "s|$UNPATCHED|a|" $target_file
+  sed -i "s|$PATCHED|b|" $target_file
 done
 
+# cabal file
 target_file=$KEEP/project_name.cabal.patch
 diff -Nur $UNPATCHED/$CABAL_FILE $PATCHED/$CABAL_FILE > $target_file
 sed -i "s|$UNPATCHED|a|" $target_file
 sed -i "s|$PATCHED|b|" $target_file
 sed -i "s|$CABAL_FILE|project_name.cabal|" $target_file
-echo "diff -urN a/project_name.cabal b/project_name.cabal" >> $combined_file
-cat $target_file >> $combined_file
 
+echo "Done."
