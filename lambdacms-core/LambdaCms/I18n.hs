06b067ff4449281b27b9a0d2ea32edae4709ed02
Arthur Fayzrakhmanov (Артур Файзрахманов) <heraldhoi@gmail.com>
2016-03-15 23:31:56 +0500

    Define HumanTimeLocale selecting function

--- lambdacms-core/LambdaCms/I18n.hs
+++ lambdacms-core/LambdaCms/I18n.hs
@@ -4,6 +4,9 @@ module LambdaCms.I18n where
 
 import           Data.Text
 import           Data.Time.Format (defaultTimeLocale, TimeLocale)
+import           Data.Time.Format.Human (HumanTimeLocale)
+import           LambdaCms.Core.Message (CoreMessage)
+import           LambdaCms.I18n.Default (defaultHumanTimeLocale)
 import           LambdaCms.I18n.Dutch
 import           LambdaCms.I18n.Italian
 import           LambdaCms.I18n.Russian
@@ -16,3 +19,14 @@ lambdaCmsTimeLocale ("it":_) = italianTimeLocale
 lambdaCmsTimeLocale ("ru":_) = russianTimeLocale
 lambdaCmsTimeLocale (_:rest) = lambdaCmsTimeLocale rest
 lambdaCmsTimeLocale []       = defaultTimeLocale
+
+-- | Provide 'HumanTimeLocale' according to given language list.
+lambdaCmsSelectHumanTimeLocale ::  [Text]
+                               -- ^ accept language list
+                               -> (CoreMessage -> String)
+                               -- ^ message render function
+                               -> HumanTimeLocale
+lambdaCmsSelectHumanTimeLocale ("ru":_) = russianHumanTimeLocale
+-- When other language will be added uncomment following line.
+-- lambdaCmsSelectHumanTimeLocale (_:rest) = lambdaCmsSelectHumanTimeLocale rest
+lambdaCmsSelectHumanTimeLocale _        = defaultHumanTimeLocale
